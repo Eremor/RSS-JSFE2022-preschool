3e5e172f915ca85369a97ec6fd6829353dcf8bd1
Eremor <artem.s13@gmail.com>
2022-01-30 22:46:20 +0300

    feat: add change portfolio works

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,9 +1,28 @@
 const burger = document.querySelector('.hamburger');
 const navigation = document.querySelector('.nav');
+const seasons = document.querySelector('.seasons');
+const portfolioButtons = seasons.querySelectorAll('.seasons__btn');
+const portfolioImages = document.querySelectorAll('.portfolio__item');
 
 const openMenu = () => {
   burger.classList.toggle('hamburger--open');
   navigation.classList.toggle('nav--open');
+}
+
+const changeActiveSeason = (event) => {
+  if(!event.target.classList.contains('btn--primary')){
+    portfolioButtons.forEach(button => button.classList.remove('btn--primary'));
+    event.target.classList.add('btn--primary');
+  }
+}
+
+const changeImage = (event) => {
+  if(event.target.classList.contains('seasons__btn')) {
+    const season = event.target.dataset.season;
+    changeActiveSeason(event);
+
+    portfolioImages.forEach((image, index) => image.src = `assets/img/${season}/${index + 1}.jpg`);
+  }
 }
 
 burger.addEventListener('click', () => openMenu());
@@ -12,3 +31,4 @@ navigation.addEventListener('click', (e) => {
     openMenu();
   }
 });
+seasons.addEventListener('click', (e) => changeImage(e))
