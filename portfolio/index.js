3cc8215773ac391e1fdbf8587e48fb7f1df17b24
Eremor <artem.s13@gmail.com>
2022-01-31 14:15:16 +0300

    feat: add change theme

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,12 +1,80 @@
 const burger = document.querySelector('.hamburger');
 const navigation = document.querySelector('.nav');
+const themeBtn = document.querySelector('.theme-btn');
 const seasons = document.querySelector('.seasons');
 const portfolioButtons = seasons.querySelectorAll('.seasons__btn');
 const portfolioImages = document.querySelectorAll('.portfolio__item');
 
+let theme = false;
+
 const openMenu = () => {
   burger.classList.toggle('hamburger--open');
   navigation.classList.toggle('nav--open');
+}
+
+const changeTheme = (theme) => {
+  const changeThemeBlocks = ['.skills', '.portfolio', '.video', '.price'];
+  const changeThemeText = ['.skills__title', '.skills__text', '.card__title', '.card__description'];
+  const changeThemeTitle = ['.section-title--primary', '.title-wrapper'];
+  
+  if(theme) {
+    changeThemeBlocks.forEach(el => { 
+      const item = document.querySelector(el);
+      item.classList.add('light-theme')
+    })
+  
+    changeThemeText.forEach(el => {
+      const items = document.querySelectorAll(el);
+      items.forEach(item => item.classList.add('light-theme--text'))
+    })
+  
+    portfolioButtons.forEach(btn => btn.classList.add('light-theme--btn'));
+  
+    changeThemeTitle.forEach(el => {
+      const items = document.querySelectorAll(el);
+      items.forEach(item => {
+        if(item.classList.contains('title-wrapper')) {
+          item.classList.add('light-theme--line')
+        } else {
+          item.classList.add('light-theme');
+          item.classList.add('light-theme--text');
+        }
+      })
+    })
+
+    themeBtn.classList.add('theme-btn--light')
+  } else {
+    changeThemeBlocks.forEach(el => { 
+      const item = document.querySelector(el);
+      item.classList.remove('light-theme')
+    })
+  
+    changeThemeText.forEach(el => {
+      const items = document.querySelectorAll(el);
+      items.forEach(item => item.classList.remove('light-theme--text'))
+    })
+  
+    portfolioButtons.forEach(btn => btn.classList.remove('light-theme--btn'));
+  
+    changeThemeTitle.forEach(el => {
+      const items = document.querySelectorAll(el);
+      items.forEach(item => {
+        if(item.classList.contains('title-wrapper')) {
+          item.classList.remove('light-theme--line')
+        } else {
+          item.classList.remove('light-theme');
+          item.classList.remove('light-theme--text');
+        }
+      })
+    })
+
+    themeBtn.classList.remove('theme-btn--light')
+  }
+}
+
+const onChangeTheme = () => {
+  theme = !theme;
+  changeTheme(theme);
 }
 
 const changeActiveSeason = (event) => {
@@ -25,10 +93,27 @@ const changeImage = (event) => {
   }
 }
 
+const setLocalStorage = () => {
+  localStorage.setItem('theme', theme);
+}
+
+const getLocalStorage = () => {
+  if(localStorage.getItem('theme')) {
+    const getTheme = localStorage.getItem('theme');
+    theme = (getTheme == 'true');
+
+    changeTheme(theme);
+  }
+}
+
 burger.addEventListener('click', () => openMenu());
 navigation.addEventListener('click', (e) => {
   if(e.target.classList == 'nav__link') {
     openMenu();
   }
 });
+themeBtn.addEventListener('click', onChangeTheme)
 seasons.addEventListener('click', (e) => changeImage(e))
+
+window.addEventListener('beforeunload', setLocalStorage)
+window.addEventListener('load', getLocalStorage)
