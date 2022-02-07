67f423c1cfe214f4e15835c8f9da266ae2bdf528
Eremor <artem.s13@gmail.com>
2022-02-07 15:16:05 +0300

    feat: add custon video player

--- custom-video/intex.js
+++ custom-video/intex.js
@@ -5,10 +5,12 @@ const video = videoPlayer.querySelector('.video__item');
 const controls = videoPlayer.querySelector('.video__controls');
 const playBtn = controls.querySelector('.video__play-btn');
 const volumeBtn = controls.querySelector('.video__volume-btn');
-const progressBar = controls.querySelector('.video__progress');
+const timeline = controls.querySelector('.video__progress');
 const volumeBar = controls.querySelector('.video__volume');
 
 let isPlay = false;
+let isMute = false;
+let isStart = true;
 let progressVideo = 0;
 
 const hiddenElements = (elements) => {
@@ -19,39 +21,84 @@ const showElements = (elements) => {
   elements.forEach(el => el.classList.remove('visually-hidden'));
 }
 
-const toggleElement = (element, className) => {
-  element.classList.toggle(className);
+const handlerVideo = () => {
+  if(isPlay) {
+    video.pause();
+    isPlay = false;
+  } else {
+    video.play();
+    isPlay = true;
+  }
+
+  playBtn.classList.toggle('video__play-btn--play');
+  startBtn.classList.toggle('visually-hidden');
 }
 
-const playVideo = () => {
-  isPlay = true;
-  console.dir(video);
-  video.currentTime = progressVideo;
-  video.play();
+const updateTimeline = () => {
+  const percent = (video.currentTime / video.duration) * 100;
+  timeline.style.backgroundSize = `${percent}% 100%`;
+  timeline.value = percent;
+
+  if(percent == 100) {
+    handlerVideo();
+    // finishVideo();
+  }
 }
 
-const pauseVideo = () => {
-  isPlay = false;
-  video.pause();
+const handlerTimeline = () => {
+  const time = (timeline.value * video.duration) / 100;
+  video.currentTime = time;
+}
+
+const mutedVolume = () => {
+  video.muted = !video.muted;
+  if(video.muted) {
+    volumeBtn.classList.add('video__volume-btn--mute')
+  } else {
+    volumeBtn.classList.remove('video__volume-btn--mute')
+    if(volumeBar.value == 0) {
+      video.volume = 0.1;
+      volumeBar.value = 1;
+      volumeBar.style.backgroundSize = '10% 100%';
+    }
+  }
+}
+
+const handlerVolume = () => {
+  if(volumeBar.value == 0) {
+    volumeBar.style.backgroundSize = '0% 100%';
+    mutedVolume();
+  } else {
+    const percent = volumeBar.value * 10;
+    volumeBar.style.backgroundSize = `${percent}% 100%`;
+    video.volume = volumeBar.value / 10;
+    video.muted = false;
+    volumeBtn.classList.remove('video__volume-btn--mute')
+  }
 }
 
 const startVideo = () => {
-  hiddenElements([poster, startBtn]);
-  showElements([video, controls]);
-  playVideo();
-}
-
-const play = () => {
-  if(isPlay) {
-    progressVideo = progressBar.value;
-    showElements([startBtn]);
-    pauseVideo();
-  } else {
-    hiddenElements([startBtn]);
-    playVideo();
+  if(isStart) {
+    showElements([video, controls]);
+    hiddenElements([poster]);
+    isStart = false;
   }
-  toggleElement(playBtn, 'video__play-btn--play');
+  isPlay = false;
+  handlerVideo();
 }
 
+const finishVideo = () => {
+  showElements([poster, startBtn]);
+  hiddenElements([video, controls]);
+  isStart = true;
+  isPlay = false;
+  playBtn.classList.remove('video__play-btn--play');
+}
+
+playBtn.addEventListener('click', handlerVideo);
 startBtn.addEventListener('click', startVideo);
-playBtn.addEventListener('click', play);
+video.addEventListener('click', handlerVideo);
+video.addEventListener('timeupdate', updateTimeline);
+timeline.addEventListener('change', handlerTimeline);
+volumeBtn.addEventListener('click', mutedVolume);
+volumeBar.addEventListener('change', handlerVolume);
